f0552e9f4fdb1af991b0a734860fd7cccc4077f0
Dedy Prasetiady <dedy.berastagi@gmail.com>
2024-07-31 10:16:53 +0700

    Tidy up handle_row_based_request implementation

--- main.py
+++ main.py
@@ -8,22 +8,17 @@ app = FastAPI()
 
 @app.post("/row-based", response_model_exclude_none=True)
 async def handle_row_based_request(
-    *, obj_in: list[schema.RowBasedInputItem]
+    row_based_input: list[schema.RowBasedInputItem],
 ) -> list[schema.RowBasedOutputItem]:
-    results: list[schema.RowBasedOutputItem] = []
-
     # Implement your custom logic here
-    for row in obj_in:
-        label = get_sentiment_analysis_label(row.text)
-        result = schema.RowBasedOutputItem(id=row.id, label=label)
-        results.append(result)
-
-    return results
+    return [
+        schema.RowBasedOutputItem(id=item.id, label=get_sentiment_label(item.text))
+        for item in row_based_input
+    ]
 
 
-def get_sentiment_analysis_label(text: str) -> str:
-    blob = TextBlob(text)
-    polarity = blob.sentiment.polarity
+def get_sentiment_label(text: str) -> str:
+    polarity = TextBlob(text).sentiment.polarity
     if polarity > 0:
         return "Positive"
     elif polarity < 0:
